c95371065c02b12c2e25041e0300efdc2f2bf3d3
ACz-UniBi <41193035+ACz-UniBi@users.noreply.github.com>
2023-09-06 15:58:26 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx_rtd_theme',
+    'sphinx.builders.latex',
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
