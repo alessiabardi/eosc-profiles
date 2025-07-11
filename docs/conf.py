66b6dea79953371af2256ca608e6dcb4ffdb4079
Alessia <alessia.bardi@gmail.com>
2025-07-11 11:48:19 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,10 +18,10 @@ import os
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#if not on_rtd:
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Project information -----------------------------------------------------
 
